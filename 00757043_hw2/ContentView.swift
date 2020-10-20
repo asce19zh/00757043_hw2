568f00fc8aa9e9f139ca120003ecbf668ded7fa8
aaaaaaaaaaa <aaaaa>
2020-10-21 05:00:20 +0800

    f

--- 00757043_hw2/ContentView.swift
+++ 00757043_hw2/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
@@ -21,9 +22,13 @@ struct TeamsView: View {
     var body:some View{
         Text("semis")
             .padding()
-        let rows = [GridItem()]
         NavigationView{
-            
+            ForEach(SemiTeams.indices){
+                (index) in
+                NavigationLink(destination:teamDetail(Teams:SemiTeams[index])) {
+                    
+                }
+            }
         }
     }
 }
@@ -52,3 +57,62 @@ TextEditor(text: $lakers_intro)
     .frame(maxHeight: 150)
     .border(Color.black)
 */
+struct teamDetail: View{
+    let Teams:Teams
+    @State private var show = false
+    var body: some View{
+        Text("a")
+    }
+}
+
+struct Teams{
+    var id = UUID()
+    let name:String
+    let Region:String
+    let createdTime:String
+    let recentMatch:String
+}
+struct Player {
+    var id = UUID()
+    let gameid:String
+    let name:String
+    let Role:String
+    let Country:String
+}
+let SemiTeams = [ Teams(name:"Suning", Region: "LPL", createdTime: "2016-12-28", recentMatch:"In the second quarterfinals of the 2020 League of Legends World Championship, the LPL third seed, Suning Gaming wiped out the LPL second seed, JD Gaming, in a dominant 3-1 series. While Suning came in as the first seed from their group, JDG's seeding, plus their group opponent - DAMWON Gaming - had many considering JDG as favorites for the match."), Teams(name:"Top Esports", Region:"LPL", createdTime: "2019-05-15", recentMatch:"Top Esports completed the first reverse sweep in League of Legends World Championship history today against Fnatic to advance to the tournament’s semifinals, where they will face Suning.The series featured high-level plays from both teams and much resilience from the LPL players who were down two games. Seen as one of the favorites to win this year’s League of Legends World Championship, TES had a shaky start against Fnatic but improved with each match."), Teams(name:"G2 Esports", Region:"LEC", createdTime: "2015-10-15", recentMatch:"After dominating performances over the past several international events, G2 Esports remains consistent, taking down Gen.G in a 3-0 sweep in the 2020 League of Legends World Championship quarterfinals. Beyond just a few troll moments, they outclassed and outplayed their opponents, making it to the semis yet again. With their dominant victory, they finish out this year's quarterfinals and will meet DAMWON Gaming in the semis for their rematch. After their series, G2 spoke with the global press to discuss their performance and upcoming match."), Teams(name:"DAMWON Gaming", Region:"LCK", createdTime: "2017-05-28", recentMatch:"The first quarterfinals series was somewhat of a slaughter, ending in a decently clean 3-0 victory for DAMWON Gaming, mimicking the LCK Summer Finals from just a few weeks ago. DWG picked apart DRX, even when they fell behind a bit here and there in a couple of early games. While each DAMWON member deserves a shoutout, the biggest difference-maker for the team was none other than Nuguri himself.")]
+
+let SN_Players = [
+    Player(gameid:"Chashao", name: "Shih Yi-Hao (史益豪)", Role:"Coach", Country:"Taiwan"),
+    Player(gameid:"Bin", name: "Chen Ze-Bin (陈泽彬)", Role:"Top Laner", Country:"China"),
+    Player(gameid:"SofM", name: "Lê Quang Duy", Role:"Jungler", Country:"Vietnam"),
+    Player(gameid:"Angel", name: "Xiang Tao (向涛)", Role:"Mid Laner", Country:"China"),
+    Player(gameid:"huanfeng", name: "Tang Huan-Feng (唐焕烽)", Role:"Bot Laner", Country:"China"),
+    Player(gameid:"SwordArt", name: "Hu Shuo-Chieh (胡碩傑)", Role:"Support", Country:"Taiwan")
+]
+
+let TES_players = [
+    Player(gameid:"BSYY", name: "Luo Sheng (罗盛)", Role:"Coach", Country:"China"),
+    Player(gameid:"369", name: "Bai Jia-Hao (白家浩)", Role:"Top Laner", Country:"China"),
+    Player(gameid:"", name: "Karsa", Role:"Jungler", Country:"Taiwan"),
+    Player(gameid:"knight", name: "Zhuo Ding (卓定)", Role:"Mid Laner", Country:"China"),
+    Player(gameid:"JackeyLove", name: "Yu Wen-Bo (喻文波)", Role:"Bot Laner", Country:"China"),
+    Player(gameid:"yuyanjia", name: "Liang Jia-Yuan (梁家源)", Role:"Support", Country:"China"),
+]
+
+let G2_players = [
+    Player(gameid:"GrabbZ", name: "Fabian Lohmann", Role:"Coach", Country:"Germany"),
+    Player(gameid:"Wunder", name: "Martin Nordahl Hansen", Role:"Top Laner", Country:"Denmark"),
+    Player(gameid:"Jankos", name: "Marcin Jankowski", Role:"Jungler", Country:"Poland"),
+    Player(gameid:"Caps", name: "Rasmus Borregaard Winther", Role:"Mid Laner", Country:"Denmark"),
+    Player(gameid:"Perkz", name: "Luka Perković", Role:"Bot Laner", Country:"Croatia"),
+    Player(gameid:"Mikyx", name: "Mihael Mehle", Role:"Support", Country:"Slovenia"),
+]
+
+let DWG_players = [
+    Player(gameid:"Zefa", name: "Lee Jae-min (이재민)", Role:"Coach", Country:"South Korea"),
+    Player(gameid:"Nuguri", name: "Jang Ha-gwon (장하권)", Role:"Top Laner", Country:"South Korea"),
+    Player(gameid:"Canyon", name: "Kim Geon-bu (김건부)", Role:"Jungler", Country:"South Korea"),
+    Player(gameid:"ShowMaker", name: "Heo Su (허수)", Role:"Mid Laner", Country:"South Korea"),
+    Player(gameid:"Ghost", name: "Jang Yong-jun (장용준)", Role:"Bot Laner", Country:"South Korea"),
+    Player(gameid:"BeryL", name: "Cho Geon-hee (조건희)", Role:"Support", Country:"South Korea"),
+]
